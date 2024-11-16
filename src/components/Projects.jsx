1af9da82262c3807e6834f4ab3a384db98bf41a1
Yasith9914 <navanjanayasith14@gmail.comm>
2024-11-16 12:46:39 +0530

    Add testimonial section and makes few changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -26,8 +26,8 @@ export default function Projects(){
         return()=>window.removeEventListener('resize',updateCardToShow)
     }, []);
     return(
-        <div className={'container mx-auto py-4 pt-20 px-6  md:px-20 lg:px-32  my:20 w-full overflow-hidden'} id={'Projects'}>
-            <h1 className={'text-2xl sm:text-4xl font-bold mb-2 text-center'}>Projects <span className={'underline underline-offset-4 decoration-1 under'}>Completed</span></h1>
+        <div className={'container mx-auto py-16 pt-20 px-6  md:px-20 lg:px-32  my:20 w-full overflow-hidden'} id={'Projects'}>
+            <h1 className={'text-2xl sm:text-4xl font-bold mb-2 text-center'}>Projects <span className={'font-light underline underline-offset-4 decoration-1 under'}>Completed</span></h1>
             <p className={'text-center text-gray-500 mx-auto mb-8 max-w-80'}>Crafting Spaces, Building Legacies—Explore Our Portfolio</p>
 
             {/*Slider Buttons*/}
@@ -50,7 +50,7 @@ export default function Projects(){
                             <div className={'absolute right-0 left-0 bottom-5 flex justify-center'}>
                                 <div className={'inline-block bg-white px-4 py-2 w-3/4 shadow-md'}>
                                     <h2 className={'text-xl font-semibold text-gray-800'}>{project.title}</h2>
-                                    <p className={'text-gray-500 text-sm'}>{project.price} <span> </span> {project.location}</p>
+                                    <p className={'text-gray-500 text-sm'}>{project.price} <span className={'px-1'}> </span> {project.location}</p>
                                 </div>
                             </div>
                         </div>
@@ -60,3 +60,4 @@ export default function Projects(){
         </div>
     )
 }
+
